3990c4845e4e1feda49ce6f6670a0bd7c65ac505
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2019-07-14 18:49:36 -0500

    Fix gallery widget

--- page-gallery.php
+++ page-gallery.php
@@ -4,7 +4,7 @@
 <div class="container space-updown">
     <div class="row">
       <div class="twelve columns">
-        <?php get_sidebar('gallery'); ?>
+        <?php dynamic_sidebar('gallery'); ?>
       </div>
     </div>
 </div>
