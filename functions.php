bea725ddff9b0e0c6a68919c5440a63e461d8352
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-14 12:40:44 -0500

    Code page.php and sidebar.php

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
   add_theme_support('post-thumbnails');
 
+//Widget Areas
+function blank_widgets_init() {
+  //Sidebar Widget
+  register_sidebar(array(
+    'name'          => ('Sidebar'),
+    'id'            => 'sidebar',
+    'description'   => 'Sidebar widget area',
+    'before_widget' => '<div class="widget-sidebar">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+}
+add_action('widgets_init', 'blank_widgets_init');
+
 ?>
