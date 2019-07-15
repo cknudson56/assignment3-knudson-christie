0aa9dd482b9f78d9cef0954b8d800d08e2a27482
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2019-07-14 20:15:55 -0500

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php /*Template Name: 404 Page*/?>
+<?php get_header(); ?>
+
+<div class="container">
+<div class="row">
+  <div class="twelve columns not-found space-updown">
+    <h2>Woof Woof!</h2>
+    <h3>That Page is Not Found</h3>
+    <p>Now if it was a frisbee, I'd be able to help you find it.</p>
+    <p>But in the meantime, would you like to go back <a href="<?php echo home_url('/');?>">HOME</a>?</p>
+  </div>
+</div>
+</div>
+
+<?php get_footer(); ?>
